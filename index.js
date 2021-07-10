8a9a22395d6305faea249af3962802438b8c198a
Maksym Leichenko <ky61k105@gmail.com>
2021-07-11 00:15:46 +0300

    feat: added codemirror

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-const textarea = ['html', 'css', 'js'];
-const textareaElements = textarea.reduce((acc, id) => ({ ...acc, [id]: document.getElementById(id) }), {});
-const textareaElementsArray = Object.values(textareaElements);
+const ids = ['html', 'css', 'js', 'output'];
+const elements = ids.reduce((acc, id) => ({ ...acc, [id]: document.getElementById(id) }), {});
 
-textareaElements.css.value = `body {
-  background: red;
-}`;
-textareaElements.js.value = `console.log(window.location.href);`;
-textareaElements.html.value = `<!DOCTYPE html>
+const editors = {
+  html: CodeMirror.fromTextArea(elements.html, {
+    lineNumbers: true,
+    mode: "htmlmixed",
+    highlightLine: true,
+  }),
+  css: CodeMirror.fromTextArea(elements.css, {
+    lineNumbers: true,
+    mode: "css",
+    highlightLine: true,
+  }),
+  js: CodeMirror.fromTextArea(elements.js, {
+    lineNumbers: true,
+    mode: "javascript",
+    highlightLine: true,
+  })
+}
+const editorsArr = Object.values(editors);
+
+editorsArr.forEach(editor => {
+  editor.on('change', onChangeHandler);
+});
+
+let timerId = null;
+function onChangeHandler() {
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+  timerId = setTimeout(render, 1000);
+}
+
+function render() {
+  let html = editors.html.getValue();
+  html = html.replace('</head>', `<style>${editors.css.getValue()}</style></head>`);
+  html = html.replace('</body>', `<script>${editors.js.getValue()}</script></body>`);
+  const iframeElement = document.createElement('iframe');
+  iframeElement.attributes.sandbox = "allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts";
+  iframeElement.attributes.frameborder = "0";
+  iframeElement.attributes.allow = "geolocation; midi; camera; microphone; speaker;";
+  iframeElement.src = "about:blank";
+  elements.output.innerHTML = '';
+  elements.output.append(iframeElement);
+  iframeElement.contentWindow.document.open();
+  iframeElement.contentWindow.document.write(html);
+  iframeElement.contentWindow.document.close();
+
+}
+
+function setDefaultValues() {
+  editors.html.setValue(`<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -15,23 +59,17 @@ textareaElements.html.value = `<!DOCTYPE html>
 </head>
 <body>
 </body>
-</html>`;
+</html>`);
 
+  editors.css.setValue(`body {
+  background: red;
+}`);
 
-const iframeElement = document.querySelector('iframe');
-
-textareaElementsArray.forEach(el => {
-  el.addEventListener('input', render);
-});
-
-function render() {
-  let html = textareaElements.html.value;
-  html = html.replace('</head>', `<style>${textareaElements.css.value}</style></head>`);
-  html = html.replace('</body>', `<script>${textareaElements.js.value}</script></body>`);
-  iframeElement.src = "about:blank";
-  iframeElement.contentWindow.document.open();
-  iframeElement.contentWindow.document.write(html);
-  iframeElement.contentWindow.document.close();
+  editors.js.setValue(`
+const a = 1;
+console.log(a);
+`);
 }
 
+setDefaultValues();
 render();
